53136805ce345a3e64cf39a9c3546ad9f1bd29e7
Paul Chiusano <paul.chiusano@gmail.com>
2014-03-07 19:16:28 -0500

    Compiling version of unfold-based process, with implementations for flatMap and onHalt

--- src/main/scala/scalaz/stream/Proc4.scala
+++ src/main/scala/scalaz/stream/Proc4.scala
@@ -1,94 +1,192 @@
-package scalaz.stream
-
-import scalaz.concurrent.Task
-import scalaz.\/
-
-trait Proc4[+F[_],+O] {
-  import Proc4._
-
-  def Match[R](
-    emit: (Seq[O], T[R]) => T[R],
-    await: HandleAwait[F,R],
-    halt: Throwable => T[R]): T[R]
-}
-
-object Proc4 {
-  type Trampoline[+A] = Task[A]
-  type T[+A] = Task[A]
-  val Trampoline = Task
-  val T = Task
-
-  trait HandleAwait[-F[_],R] {
-    def apply[Z](req: F[Z], recv: Throwable \/ Z => T[R]): T[R]
-  }
-
-  def fail(err: Throwable): Proc4[Nothing,Nothing] = new Proc4[Nothing,Nothing] {
-    def Match[R](
-      emit: (Seq[Nothing], T[R]) => T[R],
-      await: HandleAwait[Nothing,R],
-      halt: Throwable => T[R]): T[R] = halt(err)
-  }
-  val halt = fail(End)
-
-  def emit[O](o: O): Proc4[Nothing,O] = emitAll(Vector(o))
-
-  def emitAll[O](out: Seq[O]): Proc4[Nothing,O] = new Proc4[Nothing,O] {
-    def Match[R](
-      e: (Seq[O], T[R]) => T[R],
-      a: HandleAwait[Nothing,R],
-      h: Throwable => T[R]): T[R] =
-      halt.Match(e,a,h).flatMap(r => e(out, Task.now(r)))
-  }
-
-  def eval[F[_],O](req: F[O]): Proc4[F,O] = new Proc4[F,O] {
-    def Match[R](
-      e: (Seq[O], T[R]) => T[R],
-      a: HandleAwait[F,R],
-      h: Throwable => T[R]): T[R] =
-      a[O](req, (res: Throwable \/ O) => T.suspend { res.fold(fail, emit).Match(e,a,h) })
-  }
-
-  def onHalt[F[_],O](p1: Proc4[F,O], p2: Throwable => Proc4[F,O]): Proc4[F,O] =
-    new Proc4[F,O] {
-      def Match[G[_]:Monad:Catchable,R](
-        fg: F ~> G,
-        e: (Seq[O], G[R]) => G[R],
-        a: HandleAwait[F,R],
-        h: Throwable => G[R]): G[R] = {
-        ??? // 'headsplode
-        p1.Match(
-          emit = (hd,tl) =>
-            tl.attempt.flatMap(_.fold(e => p2(e).Match(e,a,h),
-                                      r => T.now(r)))
-        // need to call p1 with some properly jiggered arguments
-        // such that p2 gets invoked
-      }
-    }
-
-  def suspend[F[_],O](p: => Proc4[F,O]): Proc4[F,O] =
-    new Proc4[F,O] {
-      def Match[R](
-        e: (Seq[O], T[R]) => T[R],
-        a: HandleAwait[F,R],
-        h: Throwable => T[R]): T[R] = Task.suspend(p.Match(e,a,h))
-    }
-
-  /**
-   * Special exception indicating normal termination due to
-   * input ('upstream') termination. An `Await` may respond to an `End`
-   * by switching to reads from a secondary source.
-   */
-  case object End extends Exception {
-    override def fillInStackTrace = this
-  }
-
-  /**
-   * Special exception indicating downstream termination.
-   * An `Await` should respond to a `Kill` by performing
-   * necessary cleanup actions, then halting.
-   */
-  case object Kill extends Exception {
-    override def fillInStackTrace = this
-  }
-
-}
+//package scalaz.stream
+//
+//import scalaz.concurrent.Task
+//import scalaz.{~>, \/, Catchable, Monad, Monoid}
+//import scalaz.syntax.monad._
+//import scalaz.syntax.monoid._
+//import \/.{left,right}
+//
+//trait Proc4[+F[_],+O] {
+//  import Proc4._
+//
+//  def Match[G[_]:Monad:Catchable,R:Monoid](
+//    emit: Seq[O] => G[R],
+//    await: (F ~> G) => G[R],
+//    halt: Throwable \/ R => G[R]): G[R]
+//}
+//
+///*
+//trait Proc5[+F[_],+O] {
+//  import Proc4._
+//
+//  def stepFoldMap[G[_]:Monad:Catchable,R:Monoid](
+//    fg: F ~> G)(f: O => G[R]): G[(R,Proc5[F,O])]
+//}
+//
+//trait Proc5[+F[_],+O] {
+//  import Proc4._
+//
+//  def runFoldMap[G[_]:Monad:Catchable,R:Monoid](
+//    fg: F ~> G)(f: O => R): G[R]
+//}
+//
+//data Stream f a
+//  = forall s. Stream s (s -> Step f a s)
+//
+//data Step f a s
+//  = Halt Err
+//  | Emit [a] s
+//  | forall z. Await (f z) (Either Err z -> s)
+//
+//instance Functor (Step f a) where ...
+//
+//emit :: a -> Stream f a
+//emit a = Stream False go where
+//  go False = Emit [a] True
+//  go True = Halt
+//
+//append :: Stream f a -> Stream f a
+//append (Stream s1 f) (Stream s2 g) = Stream (Just s1, s2) go where
+//  go (Nothing, s2) = (,) Nothing <$> g s2
+//  go (Just s1, s2) = h s2 <$> f s1
+//  h s2 s1 = (Just s1, s2)
+//
+//map :: (a -> b) -> Stream f a -> Stream f b
+//map f (Stream s1 g) = Stream s1 (go . g) where
+//  go Emit as s = Emit (fmap f as) s
+//  go h@(Halt err) = Halt err
+//  go a@(Await req recv) = a
+//
+//empty :: Stream f a
+//empty = Stream () (const (Halt err))
+//
+//concat :: Stream f (Stream f a) -> Stream f a
+//concat (Stream s f) = Stream (s, Nothing) go where
+//  go :: (s, Maybe (Stream f a))
+//  go (s, Nothing) = case f s of
+//    Halt err -> Halt err
+//    Emit streams so = Emit [] (so, Just (foldr append empty streams))
+//    Await req g = Await req (\s -> (s, Nothing) . g)
+//
+//eval :: f a -> Stream f a
+//eval f = Stream (Nothing, False) go where
+//  go (Nothing, False) = Await f (\a -> (Just a, True))
+//  go (Just a) = Emit [a] (Nothing, True)
+//  go (_, True) = Halt err
+//
+//onHalt :: Stream f a -> (Err -> Stream f a) -> Stream f a
+//onHalt = -- trivial, just like append; state is (Maybe s, Either (Err -> Stream f a, Stream f a))
+//
+//yonedify the process - Proc f a = Proc1 a b -> Proc f b
+//this would mean proc1 and proc are not the same type, though,
+//or would it?
+//proc can be run in one of two modes - step, or stream
+//
+//data Proc f o =
+//  Proc
+//    (
+//
+//*/
+//
+//object Proc4 {
+//  type Trampoline[+A] = Task[A]
+//  type T[+A] = Task[A]
+//  val Trampoline = Task
+//  val T = Task
+//
+//  trait HandleAwait[-F[_],R] {
+//    def apply[G[_]:Monad:Catchable,Z](req: F[Z]): G[R]
+//  }
+//
+//  private def attempt[G[_]:Catchable,O](g: G[O]): G[Throwable \/ O] =
+//    Catchable[G].attempt(g)
+//
+//  def fail(err: Throwable): Proc4[Nothing,Nothing] =
+//    new Proc4[Nothing,Nothing] {
+//      def Match[G[_]:Monad:Catchable,R:Monoid](
+//        e: Seq[Nothing] => G[R],
+//        a: (F ~> G) => G[R],
+//        h: Throwable \/ R => G[R]): G[R] = h(left(err))
+//    }
+//
+//  val halt = fail(End)
+//
+//  def emit[O](o: O): Proc4[Nothing,O] = emitAll(Vector(o))
+//
+//  def emitAll[O](out: Seq[O]): Proc4[Nothing,O] = new Proc4[Nothing,O] {
+//    def Match[G[_]:Monad:Catchable,R:Monoid](
+//      e: Seq[O] => G[R],
+//      a: (Nothing ~> G) => G[R],
+//      h: Throwable \/ R => G[R]): G[R] =
+//      attempt(e(out)).flatMap(h)
+//  }
+//
+//  def eval[F[_],O](req: F[O]): Proc4[F,O] = new Proc4[F,O] {
+//    def Match[G[_]:Monad:Catchable,R:Monoid](
+//      e: Seq[O] => G[R],
+//      a: (F ~> G) => G[R],
+//      h: Throwable \/ R => G[R]): G[R] =
+//      a(req)
+//  }
+//
+//  def suspendM[F[_]:Monad,O](f: => F[O]): F[O] =
+//    ().point.flatMap(_ => f)
+//
+//  def onHalt[F[_],O](p1: Proc4[F,O], p2: Throwable => Proc4[F,O]): Proc4[F,O] =
+//    new Proc4[F,O] {
+//      def Match[G[_]:Monad:Catchable,R:Monoid](
+//        e: Seq[O] => G[R],
+//        a: F[O] => G[R],
+//        h: Throwable \/ R => G[R]): G[R] = suspendM[G,R] {
+//        attempt(p1.Match(e,a,h)).flatMap(
+//          _.fold(err => p2(err).Match(e,a,h),
+//                 r1 => p2(End).Match(e,a,h).map(r2 => r1 |+| r2))
+//        )
+//      }
+//    }
+//  def append[F[_],O](p1: Proc4[F,O], p2: => Proc4[F,O]): Proc4[F,O] =
+//    onHalt(p1, { case End => p2; case err => fail(err)})
+//
+//  def bind[F[_],O,O2](p1: Proc4[F,O])(f: O => Proc4[F,O2]): Proc4[F,O2] =
+//    new Proc4[F,O2] {
+//      def Match[G[_]:Monad:Catchable,R:Monoid](
+//        e: Seq[O2] => G[R],
+//        a: (F ~> G) => G[R],
+//        h: Throwable \/ R => G[R]): G[R] = suspendM[G,R] {
+//        p1.Match(
+//          emit = os => os.map(f).reverse.foldLeft(halt: Proc4[F,O2])(
+//                         (tl,hd) => append(hd,tl)
+//                       ).Match(e,a,h),
+//          await = (fg: F ~> G) o: F[O]) =>
+//          err => h(err)
+//        )
+//      }
+//    }
+////
+////  def suspend[F[_],O](p: => Proc4[F,O]): Proc4[F,O] =
+////    new Proc4[F,O] {
+////      def Match[R](
+////        e: (Seq[O], T[R]) => T[R],
+////        a: HandleAwait[F,R],
+////        h: Throwable => T[R]): T[R] = Task.suspend(p.Match(e,a,h))
+////    }
+//
+//  /**
+//   * Special exception indicating normal termination due to
+//   * input ('upstream') termination. An `Await` may respond to an `End`
+//   * by switching to reads from a secondary source.
+//   */
+//  case object End extends Exception {
+//    override def fillInStackTrace = this
+//  }
+//
+//  /**
+//   * Special exception indicating downstream termination.
+//   * An `Await` should respond to a `Kill` by performing
+//   * necessary cleanup actions, then halting.
+//   */
+//  case object Kill extends Exception {
+//    override def fillInStackTrace = this
+//  }
+//
+//}
--- src/main/scala/scalaz/stream/Proc6.scala
+++ src/main/scala/scalaz/stream/Proc6.scala
@@ -18,46 +18,77 @@ sealed trait Proc6[+F[_],+O] {
   import Proc6._
   import Step._
 
-  def onHalt[G[_],O2>:O](tl: Throwable => Proc6[G,O2])(implicit S: Sub1[F,G]):
+  def onHalt[G[_],O2>:O](tl: Throwable => Proc6[G,O2])(implicit E: Sub1[F,G]):
     Proc6[G,O2]
+
+  def ++[G[_],O2>:O](tl: => Proc6[G,O2])(implicit E: Sub1[F,G]): Proc6[G,O2] =
+    E.subprocess(this).onHalt {
+      case End => tl
+      case err => fail(err)
+    }
+
+  def onComplete[G[_],O2>:O](tl: => Proc6[G,O2])(implicit E: Sub1[F,G]): Proc6[G,O2] =
+    E.subprocess(this).onHalt {
+      case End => tl
+      case err => tl.onHalt { err2 => fail(CausedBy(err2, err)) }
+    }
+
+  def map[O2](f: O => O2): Proc6[F,O2]
+
+  def step: Step[F,Proc6[F,O],O]
+
+  def flatMap[G[_],O2](f: O => Proc6[G,O2])(implicit E: Sub1[F,G]): Proc6[G,O2] = {
+    val p: Proc6[G,Proc6[G,O2]] = E.subprocess(this).map(f)
+    unfold(p -> (halt: Proc6[G,O2])) { p =>
+      val outer = p._1; val inner = p._2
+      inner.step.fold(
+        { case End => outer.step.fold(
+            erro => Halt(erro),
+            (h,t) => if (h.isEmpty) Emit(Vector.empty, (t -> halt))
+                     else Emit(Vector.empty, cons(h.tail, t) -> h.head),
+            (req,recv) => Await(req, recv andThen (_ -> halt))
+          )
+          case erri => Halt(erri)
+        },
+        (head, tail) => Emit(head, outer -> tail),
+        (req,recv) => Await(req, recv andThen (outer -> _))
+      )
+    }
+  }
 }
 
 object Proc6 {
   import Step._
 
   type OnHaltS[F[_],+S,S2,O] = S \/ (S2, S2 => Step[F,S2,O])
+  type JoinS[F[_],+S,O] = (S, Proc6[F,O])
 
   case class Unfold[+F[_],S,+O](seed: S, next: S => Step[F,S,O]) extends Proc6[F,O] {
-    def onHaltU[G[_],S2,O2>:O](tl: Throwable => Unfold[G,S2,O2])(implicit S: Sub1[F,G]):
+    def onHaltU[G[_],S2,O2>:O](tl: Throwable => Unfold[G,S2,O2])(implicit E: Sub1[F,G]):
     Unfold[G,OnHaltS[G,S,S2,O2],O2] = {
-      Unfold(left(seed), _.fold(
-        s0 => S.substep(next(s0)).onHalt(tl),
+      Unfold(left(seed), e => Step.safe { e.fold(
+        s0 => E.substep(next(s0)).onHalt(tl),
         { p => p._2(p._1).mapS(s2 => right(s2 -> p._2)) }
-      ))
+      )})
     }
-    def onHalt[G[_],O2>:O](tl: Throwable => Proc6[G,O2])(implicit S: Sub1[F,G]):
+    def onHalt[G[_],O2>:O](tl: Throwable => Proc6[G,O2])(implicit E: Sub1[F,G]):
       Proc6[G,O2] = onHaltU(tl.asInstanceOf[Throwable => Unfold[G,Any,O2]])
+
+    def map[O2](f: O => O2): Unfold[F,S,O2] = Unfold[F,S,O2](seed, next andThen (_.map(f)))
+
+    def flatMapU[G[_],O2>:O](tl: O => Proc6[G,O2])(implicit E: Sub1[F,G]): Unfold[G,JoinS[G,S,O2],O2] =
+      ???
+
+    def step: Step[F,Proc6[F,O],O] = Step.safe {
+      next(seed) match {
+        case h@Halt(_) => h
+        case Await(req,recv) => Await(req, recv andThen (seed2 => unfold(seed2)(next)))
+        case Emit(hd,tl) => Emit(hd, unfold(tl)(next))
+      }
+    }
   }
 
-  sealed trait Sub1[-F[_],+G[_]] {
-    def apply[A](f: F[A]): G[A]
-    def subprocess[A](p: Proc6[F,A]): Proc6[G,A] =
-      Sub1.subst[F, G, ({type f[g[_]] = Proc6[g,A]})#f](p)(this)
-    def substep[S,A](p: Step[F,S,A]): Step[G,S,A] =
-      Sub1.subst[F, G, ({type f[g[_]] = Step[g,S,A]})#f](p)(this)
-  }
-  object Sub1 {
-    implicit def refl[F[_]]: Sub1[F,F] = new Sub1[F,F] {
-      def apply[A](f: F[A]): F[A] = f
-    }
-    implicit def nothing[F[_]]: Sub1[Nothing,F] = new Sub1[Nothing,F] {
-      def apply[A](f: Nothing): F[A] = f
-    }
-    def subst[F[_],G[_],Ctx[_[_]]](ctx: Ctx[F])(implicit S: Sub1[F,G]): Ctx[G] =
-      ctx.asInstanceOf[Ctx[G]]
-  }
-
-  trait Step[+F[_],+S,+O] {
+  sealed trait Step[+F[_],+S,+O] {
     def map[O2](f: O => O2): Step[F,S,O2] = this match {
       case h@Halt(_) => h
       case Emit(h,t) => safe { Emit(h.map(f), t) }
@@ -65,11 +96,16 @@ object Proc6 {
     }
     def mapS[S2](f: S => S2): Step[F,S2,O] = this match {
       case h@Halt(_) => h
-      case Emit(h, t) => safe { Emit(h, t.map(f)) }
-      case Await(req,recv) => Await(req, recv andThen (_.map(f)))
+      case Emit(h, t) => safe { Emit(h, f(t)) }
+      case Await(req,recv) => Await(req, recv andThen f)
     }
     def onHalt[G[_],S2,O2>:O](f: Throwable => Unfold[G,S2,O2])(implicit E: Sub1[F,G]):
       Step[G,OnHaltS[G,S,S2,O2],O2]
+    def flatMap[G[_],O2](f: O => Proc6[G,O2])(implicit E: Sub1[F,G]):
+      Step[G,JoinS[G,S,O2],O2]
+    def fold[R](halt: Throwable => R,
+                emit: (Seq[O], S) => R,
+                await: (F[Any], (Throwable \/ Any) => S) => R): R
   }
   object Step {
     def safe[F[_],S,O](s: => Step[F,S,O]): Step[F,S,O] =
@@ -80,58 +116,51 @@ object Proc6 {
       def onHalt[G[_],S2,O2>:Nothing](f: Throwable => Unfold[G,S2,O2])(implicit E: Sub1[Nothing,G]):
         Step[G,OnHaltS[G,Nothing,S2,O2],O2] = Step.safe {
           val tl = f(cause)
-          Emit(Seq(), Some(right(tl.seed -> tl.next)))
+          Emit(Seq(), right(tl.seed -> tl.next))
         }
+      def flatMap[G[_],O2](f: Nothing => Proc6[G,O2])(implicit E: Sub1[Nothing,G]): Step[G,JoinS[G,Nothing,O2],O2] =
+        this
+      def fold[R](halt: Throwable => R,
+                  emit: (Seq[Nothing], Nothing) => R,
+                  await: (Nothing, (Throwable \/ Any) => Nothing) => R): R = halt(cause)
     }
-    case class Emit[S,+O](head: Seq[O], tail: Option[S]) extends Step[Nothing,S,O] {
+    case class Emit[S,+O](head: Seq[O], tail: S) extends Step[Nothing,S,O] {
       def onHalt[G[_],S2,O2>:O](f: Throwable => Unfold[G,S2,O2])(implicit E: Sub1[Nothing,G]):
         Step[G,OnHaltS[G,S,S2,O2],O2] =
-          Emit(head, tail.map(left))
+          Emit(head, left(tail))
+      def flatMap[G[_],O2](f: O => Proc6[G,O2])(implicit E: Sub1[Nothing,G]): Step[G,JoinS[G,S,O2],O2] =
+        Step.safe {
+          val hds: Proc6[G,O2] = head.map(f).reverse.foldLeft(halt: Proc6[G,O2])((tl,hd) => hd ++ tl)
+          ???
+          // unfold(tail -> hds)
+        }
+      def fold[R](halt: Throwable => R,
+                  emit: (Seq[O], S) => R,
+                  await: (Nothing, (Throwable \/ Any) => S) => R): R = emit(head, tail)
     }
-    case class Await[F[_],Z,S](req: F[Z], recv: Throwable \/ Z => Option[S]) extends Step[F,S,Nothing] {
+    case class Await[F[_],Z,S](req: F[Z], recv: Throwable \/ Z => S) extends Step[F,S,Nothing] {
       def onHalt[G[_],S2,O2>:Nothing](f: Throwable => Unfold[G,S2,O2])(implicit E: Sub1[F,G]):
-        Step[G,OnHaltS[G,S,S2,O2],O2] = Await(E(req), recv andThen (_.map(left)))
+        Step[G,OnHaltS[G,S,S2,O2],O2] = Await(E(req), recv andThen left)
+      def flatMap[G[_],O2](f: Nothing => Proc6[G,O2])(implicit E: Sub1[F,G]): Step[G,JoinS[G,S,O2],O2] =
+        E.substep(this.mapS(s => (s, halt)))
+      def fold[R](halt: Throwable => R,
+                  emit: (Seq[Nothing], S) => R,
+                  await: (F[Any], (Throwable \/ Any) => S) => R): R =
+        await(req.asInstanceOf[F[Any]], recv.asInstanceOf[Throwable \/ Any => S])
     }
   }
+
+  def emitAll[O](o: Seq[O]): Proc6[Nothing,O] =
+    unfold(false) {
+      case false => Emit(o, true)
+      case true => Halt(End)
+    }
+
+  def emit[O](o: O): Proc6[Nothing,O] =
+    emitAll(Vector(o))
 
   def cons[F[_],O](head: Seq[O], tail: Proc6[F,O]): Proc6[F,O] =
-    ???
-
-  //trait Spine[+F[_],+S,+O] {
-  //  def head: S
-  //  def tail: Throwable => Proc6[F,O]
-  //}
-  //object Spine {
-  //  def apply[F[_],S,O](hd: S, tl: Throwable => Proc6[F,O]): Spine[F,S,O] =
-  //    trait Spine[+F[_],+S,+O] {
-  //      def head = hd
-  //      def tail = tl
-  //    }
-  //}
-
-  // need to be super explicit about state evolution
-  // for onHalt, we have three states -
-  // data OnHaltS f s o
-  //   = Head s (s -> Proc f o) | Got (Err -> Proc f o) | forall s . Tail s (s -> Step f s o)
-  //def join[F[_],O](p: Proc6[F,Proc6[F,O]]): Proc6[F,O] = p match {
-  //  case Unfold(z,f) => Unfold(Spine)
-  //}
-
-  // state is Either (s, Err => Proc f o) (s, s -> Step f s o)
-  def onHalt[F[_],O](p1: Proc6[F,O])(f: PartialFunction[Throwable, Proc6[F,O]]):
-  Proc6[F,O] = {
-    type S = (Any, PartialFunction[Throwable, Proc6[F,O]]) \/
-             (Any, Any => Step[F,Any,O])
-    p1 match {
-      case Unfold(seed,g) => ???
-        //def go(s: S) =
-        //  s.fold(
-        //    { case (s1, f) => g(s1).mapS(a => left(a -> f): S) },
-        //    { case (s2, g2) => g2(s2).mapS(a => right(a -> g2)) }
-        //  )
-        //go(left(seed -> f))
-    }
-  }
+    emitAll(head) ++ tail
 
   def unfold[F[_],S,O](seed: S)(f: S => Step[F,S,O]): Proc6[F,O] =
     Unfold(seed, f)
@@ -144,7 +173,7 @@ object Proc6 {
   private[stream] val kill = fail(Kill)
 
   def suspend[F[_],O](p: => Proc6[F,O]): Proc6[F,O] =
-    onHalt(fail(End)) { case End => p }
+    halt ++ p
 
   def lazily[F[_],O](p: => Proc6[F,O]): Proc6[F,O] = {
     lazy val pmemo = p
@@ -168,6 +197,36 @@ object Proc6 {
   case object Kill extends Exception {
     override def fillInStackTrace = this
   }
+
+  class CausedBy(top: Throwable, cause: Throwable) extends Throwable {
+    override def fillInStackTrace = this
+  }
+  object CausedBy {
+    def apply(top: Throwable, cause: Throwable): Throwable = top match {
+      case End => cause
+      case _ => cause match {
+        case End => top
+        case _ => new CausedBy(top, cause)
+      }
+    }
+  }
+
+  sealed trait Sub1[-F[_],+G[_]] {
+    def apply[A](f: F[A]): G[A]
+    def subprocess[A](p: Proc6[F,A]): Proc6[G,A] =
+      Sub1.subst[F, G, ({type f[g[_]] = Proc6[g,A]})#f](p)(this)
+    def substep[S,A](p: Step[F,S,A]): Step[G,S,A] =
+      Sub1.subst[F, G, ({type f[g[_]] = Step[g,S,A]})#f](p)(this)
+  }
+  object Sub1 {
+    implicit def refl[F[_]]: Sub1[F,F] = new Sub1[F,F] {
+      def apply[A](f: F[A]): F[A] = f
+    }
+    implicit def nothing[F[_]]: Sub1[Nothing,F] = new Sub1[Nothing,F] {
+      def apply[A](f: Nothing): F[A] = f
+    }
+    def subst[F[_],G[_],Ctx[_[_]]](ctx: Ctx[F])(implicit E: Sub1[F,G]): Ctx[G] =
+      ctx.asInstanceOf[Ctx[G]]
+  }
 }
 
-// vim: set ts=4 sw=4 et:
